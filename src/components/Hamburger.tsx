2e7a45566aa49fe2c5ae2d3bc32e31587ff1d95a
Ryan Wong <ryan.suzuki.w@gmail.com>
2023-07-15 17:39:24 -0700

    added useState for menu open/close. tweaked hamburger svg

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,23 +1,23 @@
-import { useState } from "preact/hooks";
 import "../styles/hamburger.css"
 
-export default function Hamburger() {
-  const [isOpen, setIsOpen] = useState(false);
+type HamburgerProps = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
+export default function Hamburger({ isOpen, setIsOpen }: HamburgerProps) {
+  // const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
     console.log("clicked. isOpen is now: " + isOpen);
   };
 
-  // border-2 border-black dark:border-white rounded
-
   return (
     <button onClick={handleClick}>
-      <svg class="hamburger" viewBox="0 0 100 100" height={30} width={30}>
-        <rect class={`top line ${isOpen ? "open" : ""}`} width="80" height="10" rx="5" x="10" y="25"></rect>
-        <rect class={`middle line ${isOpen ? "open": ""}`} width="80" height="10" rx="5" x="10" y="45"></rect>
-        <rect class={`bottom line ${isOpen ? "open" : ""}`} width="80" height="10" rx="5" x="10" y="65 "></rect>
-
+      <svg class="hamburger" viewBox="0 0 100 100" height={40} width={40}>
+        <rect class={`top line fill-gray-600 dark:fill-gray-300 ${isOpen ? "open" : ""}`} width="80" height="10" rx="5" x="10" y="25"></rect>
+        <rect class={`middle line fill-gray-600 dark:fill-gray-300 ${isOpen ? "open" : ""}`} width="80" height="10" rx="5" x="10" y="45"></rect>
+        <rect class={`bottom line fill-gray-600 dark:fill-gray-300 ${isOpen ? "open" : ""}`} width="80" height="10" rx="5" x="10" y="65 "></rect>
       </svg>
     </button>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
+import { useState } from "preact/hooks";
+
 import Hamburger from "./Hamburger";
 import ThemeToggle from "./ThemeToggle";
+
 
 export default function Header() {
 
@@ -10,43 +13,62 @@ export default function Header() {
     ["#contact", "Contact"],
   ];
 
+  const [isOpen, setIsOpen] = useState(false);
+  
 
   return (
     <header class="fixed top-0 bg-white dark:bg-slate-600 z-10 w-full bg-gray-100">
-      <nav class="relative container mx-auto p-3">
-        <div class="flex items-center justify-end gap-8">
-          {/* <!-- Logo --> */}
-          <a href="#" class="mr-auto flex items-center space-x-2">
-            {/* <!-- <img src="terminalIcon.svg" alt="Terminal Icon" class="dark:stroke-white"> --> */}
+      <nav class="flex items-center justify-end gap-x-8 relative px-12 mx-auto h-12">
 
-              {/* <!-- is class dark:fill-white working? --> */}
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                <path
-                  fill="currentColor"
-                  d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16c1.1 0 2-.9 2-2V6a2 2 0 0 0-2-2zm0 14H4V8h16v10zm-8-2c0-.55.45-1 1-1h4c.55 0 1 .45 1 1s-.45 1-1 1h-4c-.55 0-1-.45-1-1zM6.79 9.71a.996.996 0 0 1 1.41 0l2.59 2.59c.39.39.39 1.02 0 1.41L8.2 16.3a.996.996 0 1 1-1.41-1.41L8.67 13l-1.88-1.88a.996.996 0 0 1 0-1.41z"
-                ></path>
-              </svg>
-              <span class="text-xl leading-none">Ryan</span>
-          </a>
+        {/* Logo + Name */}
+        <a href="#" class="mr-auto flex items-center space-x-2">
 
-          {/* <!-- Nav --> */}
-          <div class="hidden md:flex space-x-6">
+          {/* is class dark:fill-white working? */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+            <path
+              fill="currentColor"
+              d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16c1.1 0 2-.9 2-2V6a2 2 0 0 0-2-2zm0 14H4V8h16v10zm-8-2c0-.55.45-1 1-1h4c.55 0 1 .45 1 1s-.45 1-1 1h-4c-.55 0-1-.45-1-1zM6.79 9.71a.996.996 0 0 1 1.41 0l2.59 2.59c.39.39.39 1.02 0 1.41L8.2 16.3a.996.996 0 1 1-1.41-1.41L8.67 13l-1.88-1.88a.996.996 0 0 1 0-1.41z"
+            ></path>
+          </svg>
+          <span class="text-xl leading-none">Ryan</span>
+        </a>
+
+        {/* Nav */}
+        <div class="hidden md:flex space-x-6">
+          {
+            navs.map((nav) => (
+              <a href={nav[0]} class="hover:text-gray-400">
+                {nav[1]}
+              </a>
+            ))
+          }
+        </div>
+
+        {/* Dark Mode Toggle */}
+
+        <div class="hidden md:flex items-center">
+          <ThemeToggle />
+        </div>
+
+        {/* Hamburger button for smaller screens */}
+        <div class="md:hidden flex items-center">
+          <Hamburger isOpen={isOpen} setIsOpen={setIsOpen}/>
+        </div>
+
+        {/* Hamburger menu for smaller screens */}
+        <div class={`md:hidden absolute top-12 left-0 w-full bg-white dark:bg-slate-600 z-20 ${isOpen ? "flex" : "hidden"}`}>
+          <div class="flex flex-1 flex-col items-center mt-12 mb-6 space-y-12">
             {
               navs.map((nav) => (
-                <a href={nav[0]} class="hover:text-gray-400">
+                <a href={nav[0]} class="text-xl hover:text-gray-400">
                   {nav[1]}
                 </a>
               ))
             }
-          </div>
-
-          {/* <!-- Dark Mode Toggle --> */}
-          <ThemeToggle />
-
-          <div class="md:hidden">
-            <Hamburger />
+            <ThemeToggle />
           </div>
         </div>
+
       </nav>
     </header>
 
